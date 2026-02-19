3458e91b00dfa88506370e3cdce0db7bd4f513c7
Capita1 <gabrielhenrik25@gmail.com>
2026-02-19 15:09:05 -0300

    estilo

--- backend/users/views.py
+++ backend/users/views.py
@@ -24,7 +24,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return redirect('login')
+    return redirect('home')
 
 def registro_view(request):
     if request.method == "POST":
